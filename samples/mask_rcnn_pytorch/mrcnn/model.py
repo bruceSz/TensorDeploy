ecd6a461a0f80b45c3598f2189d2a60a6a34e668
杨云燕 <yangyunyan@yangyunyandeMacBook-Pro.local>
2023-04-10 22:49:08 +0800

    add mask-rcnn first commit
    Signed-off-by: 杨云燕 <yangyunyan@yangyunyandeMacBook-Pro.local>

---
+++ samples/mask_rcnn_pytorch/mrcnn/model.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+Mask R-CNN
+The main model implementation.
+
+Copyright (c) 2023 X, Inc.
+Licensed under the MIT License (see LICENSE for details)
+Written by bruceSz
+"""
+
+def main():
+    pass
+if  __name__ == "__main__":
+    main()
