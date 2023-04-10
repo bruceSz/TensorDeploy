b3e2d30ca1fbc40360ffb742c12f88a2bef9d3a9
zsyuyizhang <zsyuyizhang@gmail.com>
2023-04-11 06:50:34 +0800

    add stage shape compute function
    Signed-off-by: zsyuyizhang <zsyuyizhang@gmail.com>

--- samples/mask_rcnn_pytorch/mrcnn/model.py
+++ samples/mask_rcnn_pytorch/mrcnn/model.py
@@ -9,6 +9,26 @@ Licensed under the MIT License (see LICENSE for details)
 Written by bruceSz
 """
 
+import math
+import numpy as np
+
+
+def compute_bb_shapes(config, img_shape):
+    """
+    Compute width and height of each stage of bb network
+    Returns:
+      [N,(height, width)]. Where N is the number of stages
+    """
+
+    if callable(config.BACKBONE):
+        return config.BACKBONE(img_shape)
+    
+    assert config.BACKBONE in ["resnet50", "resnet101"]
+    return np.array([
+        [int(math.ceil(img_shape[0]/stride)),
+         int(math.ceil(img_shape[1]/stride))]
+        for stride in config.BACKBONE_STRIDES])
+
 def main():
     pass
 if  __name__ == "__main__":
